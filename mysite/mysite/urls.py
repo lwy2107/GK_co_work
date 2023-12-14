95164402d736b84a76caef1b091bc457561e9281
lwy2107 <bh2107@naver.com>
2023-12-14 10:00:49 +0900

    장고 제작했던거

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from mysite.views import HomeView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # shkim
+    path('', HomeView.as_view(), name='home'),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
+]
+
